e7a8d5b76f0ec35cb1a992f25dc24a25592a52d5
hubert-thieriot <hubert.thieriot@gmail.com>
2026-04-10 18:13:28 +0100

    -adding build_national argument

--- build_data.R
+++ build_data.R
@@ -6,8 +6,10 @@ build_data <- function(min_year = 2000,
                        edgar_map_years = 2022,
                        provincial_iso2s=c("ID", "IN", "ZA", "TH", "CN"),
                        sources = c("CEDS", "EDGAR"),
+                       build_national = TRUE,
                        build_maps = TRUE,
-                       build_provincial = TRUE) {
+                       build_provincial = TRUE,
+                       keep_raw_cache = FALSE) {
   sources <- toupper(sources)
   results <- list()
 
@@ -15,10 +17,12 @@ build_data <- function(min_year = 2000,
 
   # Build CEDS data if requested
   if ("CEDS" %in% sources) {
-    message("Building CEDS emissions data...")
-    ceds <- CEDSNational$new()
-    ceds$build(min_year = min_year)
-    results$CEDS <- ceds
+    if (build_national) {
+      message("Building CEDS emissions data...")
+      ceds <- CEDSNational$new()
+      ceds$build(min_year = min_year)
+      results$CEDS <- ceds
+    }
 
     # Build provincial if requested
     if (build_provincial) {
@@ -29,17 +33,23 @@ build_data <- function(min_year = 2000,
       # Build maps if requested
       if (build_maps) {
         message("Building CEDS map data...")
-        ceds_provincial$map_source$build(years = ceds_map_years, formats = c("cog"))
+        ceds_provincial$map_source$build(
+          years = ceds_map_years,
+          formats = c("cog"),
+          keep_raw_cache = keep_raw_cache
+        )
       }
     }
   }
 
   # Build EDGAR data if requested
   if ("EDGAR" %in% sources) {
-    message("Building EDGAR emissions data...")
-    edgar <- EDGARNational$new()
-    edgar$build(min_year = min_year)
-    results$EDGAR <- edgar
+    if (build_national) {
+      message("Building EDGAR emissions data...")
+      edgar <- EDGARNational$new()
+      edgar$build(min_year = min_year, keep_raw_cache = keep_raw_cache)
+      results$EDGAR <- edgar
+    }
 
     # Build provincial if requested
     if (build_provincial) {
@@ -50,7 +60,11 @@ build_data <- function(min_year = 2000,
       # Build maps if requested
       if (build_maps) {
         message("Building EDGAR map data...")
-        edgar_provincial$map_source$build(years = edgar_map_years, formats = c("cog"))
+        edgar_provincial$map_source$build(
+          years = edgar_map_years,
+          formats = c("cog"),
+          keep_raw_cache = keep_raw_cache
+        )
       }
     }
   }
